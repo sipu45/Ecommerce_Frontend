b8c9258a25b899cf0d011fa633fe895db5116e23
Sipu Padhiari <sipupadhiari6146@gmail.com>
2026-09-20 23:27:54 +0530

    get a new error CORS

--- src/store/reducers/ProductReducer.js
+++ src/store/reducers/ProductReducer.js
@@ -5,5 +5,27 @@ const initialState = {
 };
 
 export const productReducer = (state = initialState , action) => {
+    switch(action.type){
+        case "FETCH_PRODUCTS":
+            return{
+                ...state,
+                products: action.payload,
+                pagination: {
+                    ...state.pagination,
+                    pageNumber: action.pageNumber,
+                    pageSize: action.pageSize,
+                    totalElements: action.totalElements,
+                    totalPages: action.totalPages,
+                    lastPage: action.lastPage
+                },
+
+            };
+
+
+        default:
+            return state;
+
+    }
+
 
 };
